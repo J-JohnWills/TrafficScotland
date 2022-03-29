df6b029b2de0af6e39a218d9700784f43126bf76
EmporerEmu <jjwilliams013@gmail.com>
2022-03-29 13:31:57 +0100

    Add ListAdapter method to HomeController
    ListAdapter method in the HomeController will
    be used to attach the info from each individual traffic
    scotland incident to a list view item.

--- app/src/main/java/jjwilliams/trafficscotland/controllers/HomeController.java
+++ app/src/main/java/jjwilliams/trafficscotland/controllers/HomeController.java
@@ -2,6 +2,7 @@ package jjwilliams.trafficscotland.controllers;
 
 // Jamie Williams : S2029548
 
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -9,20 +10,27 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 import jjwilliams.trafficscotland.R;
 import jjwilliams.trafficscotland.data.TrafficScotlandPullParser;
 import jjwilliams.trafficscotland.models.TrafficScotlandFeed;
+import jjwilliams.trafficscotland.models.TrafficScotlandItem;
 
 public class HomeController extends Fragment {
 
@@ -32,44 +40,73 @@ public class HomeController extends Fragment {
   TrafficScotlandFeed trafficScotlandFeed = new TrafficScotlandFeed();
   TrafficScotlandPullParser pullParser = new TrafficScotlandPullParser();
 
+  private ListView listView;
+
 
   public View onCreateView(@NonNull LayoutInflater inflater,
                            ViewGroup container, Bundle savedInstanceState) {
 
 
-    startProgress();
+    populateCards();
     View root = inflater.inflate(R.layout.fragment_home, container, false);
+    listView = root.findViewById(R.id.home_list_view);
     return root;
   }
 
-  public void startProgress() {
-    executor.execute(new Runnable() {
-      @Override
-      public void run() {
-        // Background work here
-        URL aurl;
-        URLConnection yc;
-        BufferedReader in = null;
+  public void populateCards() {
+    executor.execute(() -> {
+      URL aurl;
+      URLConnection yc;
+      BufferedReader in = null;
 
-        try {
-          aurl = new URL("https://trafficscotland.org/rss/feeds/currentincidents.aspx");
-          yc = aurl.openConnection();
-          in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
-          pullParser.setInputStream(yc.getInputStream());
-          trafficScotlandFeed = pullParser.parseTrafficScotlandFeed();
-          Log.i("Traffic Scotland feed item", trafficScotlandFeed.toString());
-        } catch (Exception e) {
-          e.printStackTrace();
-        }
+      try {
+        aurl = new URL("https://trafficscotland.org/rss/feeds/roadworks.aspx");
+        yc = aurl.openConnection();
+        in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+        pullParser.setInputStream(yc.getInputStream());
+        trafficScotlandFeed = pullParser.parseTrafficScotlandFeed();
+        Log.i("Traffic Scotland feed item", trafficScotlandFeed.toString());
 
-        handler.post(new Runnable() {
-          @Override
-          public void run() {
-            // UI thread here
-          }
-        });
+      } catch (Exception e) {
+        e.printStackTrace();
       }
+
+      handler.post(() -> {
+        ListAdapter adapter = new ListAdapter(this.getContext(), trafficScotlandFeed.getTrafficScotlandItems());
+        listView.setAdapter(adapter);
+      });
     });
+  }
+
+  class ListAdapter extends ArrayAdapter<TrafficScotlandItem> {
+
+    private Context context;
+    private ArrayList<TrafficScotlandItem> trafficScotlandItems;
+
+    public ListAdapter(Context context, ArrayList<TrafficScotlandItem> trafficScotlandItems) {
+      super(context, R.layout.traffic_scotland_item_card, trafficScotlandItems);
+      this.context = context;
+      this.trafficScotlandItems = trafficScotlandItems;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+      LayoutInflater inflater = (LayoutInflater) context.getApplicationContext()
+              .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+      View trafficScotlandItemCard = inflater.inflate(R.layout.traffic_scotland_item_card, parent,
+              false);
+      ImageView currentImage = trafficScotlandItemCard.findViewById(R.id.image);
+      TextView currentTitle = trafficScotlandItemCard.findViewById(R.id.textView1);
+      TextView currentDescription = trafficScotlandItemCard.findViewById(R.id.textView2);
+      TextView currentType = trafficScotlandItemCard.findViewById(R.id.home_card_type_text);
+
+      currentTitle.setText(trafficScotlandItems.get(position).getTitle());
+      currentDescription.setText(trafficScotlandItems.get(position).getDescription());
+
+      return trafficScotlandItemCard;
+    }
   }
 
   @Override
