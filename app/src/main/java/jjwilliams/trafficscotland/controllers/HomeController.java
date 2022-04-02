fc4faae0f8b95940f12dabfc7b008501304b4cf2
EmporerEmu <jjwilliams013@gmail.com>
2022-04-02 17:54:33 +0100

    Add filter functionality to home controller
    Users are now able to filter accident type using the button toggle
    group.

--- app/src/main/java/jjwilliams/trafficscotland/controllers/HomeController.java
+++ app/src/main/java/jjwilliams/trafficscotland/controllers/HomeController.java
@@ -2,6 +2,7 @@ package jjwilliams.trafficscotland.controllers;
 
 // Jamie Williams : S2029548
 
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
@@ -14,15 +15,20 @@ import android.widget.ListView;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.button.MaterialButtonToggleGroup;
+
 import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 import jjwilliams.trafficscotland.R;
 import jjwilliams.trafficscotland.adapters.HomeListAdapter;
 import jjwilliams.trafficscotland.data.TrafficScotlandController;
 import jjwilliams.trafficscotland.models.TrafficScotlandFeed;
 import jjwilliams.trafficscotland.models.TrafficScotlandItem;
+import jjwilliams.trafficscotland.models.TrafficScotlandType;
 
 public class HomeController extends Fragment {
 
@@ -32,16 +38,60 @@ public class HomeController extends Fragment {
   TrafficScotlandFeed trafficScotlandFeed = new TrafficScotlandFeed();
   ArrayList<TrafficScotlandItem> trafficScotlandItems = new ArrayList<>();
 
-
   private ListView listView;
+  MaterialButtonToggleGroup materialButtonToggleGroup;
 
   public View onCreateView(@NonNull LayoutInflater inflater,
                            ViewGroup container, Bundle savedInstanceState) {
 
+    Context context = this.getContext();
 
     connectorTest();
+
     View root = inflater.inflate(R.layout.fragment_home, container, false);
     listView = root.findViewById(R.id.home_list_view);
+    materialButtonToggleGroup = root.findViewById(R.id.home_toggle_button_group);
+    int buttonId = materialButtonToggleGroup.getCheckedButtonId();
+    MaterialButton button = materialButtonToggleGroup.findViewById(buttonId);
+
+    materialButtonToggleGroup.addOnButtonCheckedListener(new MaterialButtonToggleGroup.OnButtonCheckedListener() {
+      @Override
+      public void onButtonChecked(MaterialButtonToggleGroup group, int checkedId, boolean isChecked) {
+        if (isChecked) {
+          if (checkedId == R.id.btnIncidents) {
+            // filter by incidents
+            ArrayList<TrafficScotlandItem> incidents = new ArrayList<>();
+            for (TrafficScotlandItem item : trafficScotlandItems) {
+              if (item.getType().equals(TrafficScotlandType.CURRENT_INCIDENT)) {
+                incidents.add(item);
+              }
+            }
+            HomeListAdapter adapter = new HomeListAdapter(context, incidents);
+            listView.setAdapter(adapter);
+
+          } else if (checkedId == R.id.btnPlanned) {
+            ArrayList<TrafficScotlandItem> planned = new ArrayList<>();
+            for (TrafficScotlandItem item : trafficScotlandItems) {
+              if (item.getType().equals(TrafficScotlandType.PLANNED_ROADWORKS)) {
+                planned.add(item);
+              }
+            }
+            HomeListAdapter adapter = new HomeListAdapter(context, planned);
+            listView.setAdapter(adapter);
+
+          } else if (checkedId == R.id.btnRoadworks) {
+            ArrayList<TrafficScotlandItem> roadworks = new ArrayList<>();
+            for (TrafficScotlandItem item : trafficScotlandItems) {
+              if (item.getType().equals(TrafficScotlandType.ROADWORKS)) {
+                roadworks.add(item);
+              }
+            }
+            HomeListAdapter adapter = new HomeListAdapter(context, roadworks);
+            listView.setAdapter(adapter);
+          }
+        }
+      }
+    });
     return root;
   }
 
